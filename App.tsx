a38fb156fba93a5bd63052e9ca0584910deee52c
Alex Martínez de Francisco <alexmartinezdefrancisco@gmail.com>
2023-08-23 21:14:42 +0200

    New setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// React imports
+import React from 'react'
+
+// Main import
+import Main from '@ui/Main'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Main/>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+    api.cache(true)
+    return {
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    'alias': {
+                        '@config': './src/config',
+                        '@data': './src/data',
+                        '@domain': './src/domain',
+                        '@ui': './src/ui'
+                    },
+                    'root': ['.']
+                }
+            ]
+        ],
+        presets: ['babel-preset-expo']
+    }
+}
